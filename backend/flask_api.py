99455802daceb19024260358eed7d0bd91d566b9
Ronald Saenz <rjsaenz@espol.edu.ec>
2023-04-21 23:24:47 -0400

    it's done

--- backend/flask_api.py
+++ backend/flask_api.py
@@ -20,8 +20,6 @@ app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 
-
-
 class NumpyEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, np.ndarray):
@@ -43,9 +41,9 @@ y_test_df = pd.read_csv(path.join(deploy_folder,"y_test_data_group_acdc.csv")).t
 def summary():
     string_list = []
     model.summary(line_length=80, print_fn=lambda x: string_list.append(x))
-    #summary_json = "\n".join(string_list)
-
-    return jsonify({'output':string_list})
+    summary_json = "\n".join(string_list)
+    
+    return jsonify({'output':summary_json})
 
 
 
@@ -66,22 +64,18 @@ def scores():
     label_dict = {'FirstYearPersistence_no': 0, 'FirstYearPersistence_yes': 1}
 
     classification_report_var = classification_report(y_test_df.argmax(axis=1), predict_labeled, target_names=label_dict)
-    print(classification_report_var)
-    #print(classification_report(y_test_df.argmax(axis=1), predict_labeled, target_names=label_dict))
 
-    #print(f'accuracy={accuracy}  precision={precision}  recall={recall}  f1={f1}  roc_auc={roc_auc}  confussion_matrix={conf_matrix} ')
-
-    res = jsonify({"accuracy": accuracy,
-                    "precision": precision,
-                    "recall":recall,
-                    "f1": f1,
-                    "roc_auc": roc_auc,
-                    "confussion_matrix": str(conf_matrix),
-                    "classification_report_var": classification_report_var
-                    })
-
-    #res.headers.add('Access-Control-Allow-Origin', '*')
-    return res
+    resJson = {
+        "accuracy": accuracy,
+        "precision": precision,
+        "recall":recall,
+        "f1": f1,
+        "roc_auc": roc_auc,
+        "confussion_matrix": str(conf_matrix),
+        "classification_report_var": classification_report_var
+    }
+    
+    return jsonify({'output':resJson})
     
 
 
@@ -89,40 +83,33 @@ def scores():
 #@cross_origin()
 def predict():
     json_dict = request.json
-    #input_vector = np.array(json_dict['data'])
-
     class_names = ['FirstTermGpa', 'SecondTermGpa', 'FirstLanguage', 'Funding', 'School', 'FastTrack', 'Coop', 'Residency', 'Gender', 'PreviousEducation', 'AgeGroup', 'HighSchoolAverageMark', 'MathScore', 'EnglishGrade']
-
-    print('Data JSON: \n', json_dict)    
+ 
     df = pd.DataFrame(json_dict, columns=class_names)
 
-    df["FirstTermGpa"] = df["FirstTermGpa"].astype(np.float32)
-    df["SecondTermGpa"] = df["SecondTermGpa"].astype(np.float32)
-    df["Funding"] = df["Funding"].astype(int)
-    df["School"] = df["School"].astype(int)
-    df["FastTrack"] = df["FastTrack"].astype(int)
-    df["Coop"] = df["Coop"].astype(int)
-    df["Residency"] = df["Residency"].astype(int)
-    df["Gender"] = df["Gender"].astype(int)
-    df["PreviousEducation"] = df["PreviousEducation"].astype(int)
-    df["AgeGroup"] = df["AgeGroup"].astype(int)
-    df["HighSchoolAverageMark"] = df["HighSchoolAverageMark"].astype(np.float32)
-    df["MathScore"] = df["MathScore"].astype(np.float32)
-    df["EnglishGrade"] = df["EnglishGrade"].astype(int)
-
+    df["FirstTermGpa"] = df["FirstTermGpa"].astype(np.float64)
+    df["SecondTermGpa"] = df["SecondTermGpa"].astype(np.float64)
+    df["Funding"] = df["Funding"].astype(np.float64)
+    df["School"] = df["School"].astype(np.float64)
+    df["FastTrack"] = df["FastTrack"].astype(np.float64)
+    df["Coop"] = df["Coop"].astype(np.float64)
+    df["Residency"] = df["Residency"].astype(np.float64)
+    df["Gender"] = df["Gender"].astype(np.float64)
+    df["PreviousEducation"] = df["PreviousEducation"].astype(np.float64)
+    df["AgeGroup"] = df["AgeGroup"].astype(np.float64)
+    df["HighSchoolAverageMark"] = df["HighSchoolAverageMark"].astype(np.float64)
+    df["MathScore"] = df["MathScore"].astype(np.float64)
+    df["EnglishGrade"] = df["EnglishGrade"].astype(np.float64)
 
     #row = [[4, 3, 1, 1, 6, 2, 1, 1, 1, 2, 3, 92, 41, 9]]
-    #row = [input_vector]
-    print(df)
+    row = [df]
     y_hat = model.predict(df)
-    
-    
+
     return json.dumps({'output':(y_hat.argmax(axis=1))}, cls=NumpyEncoder)
 
 
 #Main method, runs at the very beginning...
 if __name__ == '__main__':
-    
     port = 12345 # If you don't provide any port the port will be set to 12345
         
     app.run(host="0.0.0.0", port=port, debug=True) #Run the app
--- frontend/script.js
+++ frontend/script.js
@@ -19,20 +19,20 @@ $(document).ready(function () {
         const englishGrade = $('#englishGrade').val();
 
         var dataJson = JSON.stringify( [{
-            "FirstTermGpa": firstTermGPA,
-            "SecondTermGpa": secondTermGPA,
-            "FirstLanguage": firstLanguage,
-            "Funding": funding,
-            "School": school,
-            "FastTrack": fastTrack,
-            "Coop": coop,
-            "Residency": residency,
-            "Gender": gender,
-            "PreviousEducation": previousEducation,
-            "AgeGroup": ageGroup,
-            "HighSchoolAverageMark": highSchoolAverageMark,
-            "MathScore": mathScore,
-            "EnglishGrade": englishGrade
+            "FirstTermGpa": parseFloat(firstTermGPA),
+            "SecondTermGpa": parseFloat(secondTermGPA),
+            "FirstLanguage": parseFloat(firstLanguage),
+            "Funding": parseFloat(funding),
+            "School": parseFloat(school),
+            "FastTrack": parseFloat(fastTrack),
+            "Coop": parseFloat(coop),
+            "Residency": parseFloat(residency),
+            "Gender": parseFloat(gender),
+            "PreviousEducation": parseFloat(previousEducation),
+            "AgeGroup": parseFloat(ageGroup),
+            "HighSchoolAverageMark": parseFloat(highSchoolAverageMark),
+            "MathScore": parseFloat(mathScore),
+            "EnglishGrade": parseFloat(englishGrade)
         }] );
 
         // Show the modal and loading spinner
@@ -54,7 +54,7 @@ $(document).ready(function () {
                     mockPrediction = result.output[0]
                             
                     // Update the result text
-                    $("#resultText").text("The predicted First Year Persistence is: " + mockPrediction);
+                    $("#resultText").html("<b>The predicted First Year Persistence is:</b> <br>" + mockPrediction);
             
                     // Hide the loading spinner and show the appropriate icon and animate it
                     $("#resultLoading").hide();
@@ -66,28 +66,93 @@ $(document).ready(function () {
                         $("#resultGood").hide();
                     }
                 }
-            });
-
-            /*
-            // Set a fixed mock prediction result
-            const mockPrediction = 1;
-            
-            alert(formData);
-
-            // Update the result text
-            $("#resultText").text("The predicted First Year Persistence is: " + mockPrediction);
-    
-            // Hide the loading spinner and show the appropriate icon and animate it
-            $("#resultLoading").hide();
-            if (mockPrediction === 1) {
-            $("#resultGood").show().addClass("animate-scale-up");
-            $("#resultSad").hide();
-            } else {
-            $("#resultSad").show().addClass("animate-scale-up");
-            $("#resultGood").hide();
-            }*/
-        
+            });        
         }, 1000); // 1000 ms (1 second) delay to simulate waiting for the API response
     });
+
+
+    
+    $("#btn_summary").click(function (event) {
+        event.preventDefault();
+
+        // Show the modal and loading spinner
+        $("#resultModal").modal("show");
+        $("#resultLoading").show();
+        $("#resultGood").hide().removeClass("animate-scale-up");
+        $("#resultSad").hide().removeClass("animate-scale-up");
+
+        // Simulate a short delay (e.g., waiting for API response)
+        setTimeout(function () {
+            $.ajax({
+                url: "http://127.0.0.1:5000/api/summary",
+                dataType: 'json',
+                type: 'GET',
+                contentType: 'application/json',
+                processData: false,
+                success: function (result,status,xhr) {
+                    mockPrediction = result.output
+                            
+                    // Update the result text
+                    $("#resultText").html("<b>The summary of the model:</b> <br>" + mockPrediction.replace(/\n/g, "<br>"));
+            
+                    // Hide the loading spinner and show the appropriate icon and animate it
+                    $("#resultLoading").hide();
+                    if (mockPrediction !== null) {
+                        $("#resultGood").show().addClass("animate-scale-up");
+                        $("#resultSad").hide();
+                    } else {
+                        $("#resultSad").show().addClass("animate-scale-up");
+                        $("#resultGood").hide();
+                    }
+                }
+            });        
+        }, 1000); // 1000 ms (1 second) delay to simulate waiting for the API response
+    });
+
+    $("#btn_scores").click(function (event) {
+        event.preventDefault();
+
+        // Show the modal and loading spinner
+        $("#resultModal").modal("show");
+        $("#resultLoading").show();
+        $("#resultGood").hide().removeClass("animate-scale-up");
+        $("#resultSad").hide().removeClass("animate-scale-up");
+
+        // Simulate a short delay (e.g., waiting for API response)
+        setTimeout(function () {
+            $.ajax({
+                url: "http://127.0.0.1:5000/api/scores",
+                dataType: 'json',
+                type: 'GET',
+                contentType: 'application/json',
+                processData: false,
+                success: function (result,status,xhr) {
+                    mockPrediction = result.output
+
+                    // Update the result text
+                    $("#resultText").html(
+                        "<b>Test accuracy:</b> " + mockPrediction.accuracy + "<br>" +
+                        "<b>Test precision:</b> " + mockPrediction.precision + "<br>" +
+                        "<b>Test recall:</b> " + mockPrediction.recall + "<br>" +
+                        "<b>Test f1:</b> " + mockPrediction.f1 + "<br>" +
+                        "<b>Test roc_auc:</b> " + mockPrediction.roc_auc + "<br>" +
+                        "<b>Test confussion_matrix:</b> <br>" + mockPrediction.confussion_matrix.replace(/\n/g, "<br>") + "<br>" +
+                        "<b>Test classification_report_var:</b> <br>" + mockPrediction.classification_report_var.replace(/\n/g, "<br>") + "<br>"
+                    );
+
+                    // Hide the loading spinner and show the appropriate icon and animate it
+                    $("#resultLoading").hide();
+                    if (mockPrediction !== null) {
+                        $("#resultGood").show().addClass("animate-scale-up");
+                        $("#resultSad").hide();
+                    } else {
+                        $("#resultSad").show().addClass("animate-scale-up");
+                        $("#resultGood").hide();
+                    }
+                }
+            });        
+        }, 1000); // 1000 ms (1 second) delay to simulate waiting for the API response
+    });
+
 });
   
